c56bd938b2cb0e14ac7ad66b68d6414b1d6fde9a
john199310 <johnwilliam199024@gmail.com>
2023-01-18 23:27:12 -0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import {HttpClientModule} from '@angular/common/http'
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService} from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroFormComponent } from './hero-form/hero-form.component'
+
+import { CommonModule } from '@angular/common';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent,
+    HeroFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    CommonModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation:false}
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // no need to place any providers due to the `providedIn` flag...
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+// import { Hero } from './hero';
+
+// export const HEROES: Hero[] = [
+//   { id: 0, name: 'Dr. Nice' },
+//   { id: 1, name: 'Bombasto' },
+//   { id: 2, name: 'Celeritas' },
+//   { id: 3, name: 'Magneta' },
+//   { id: 4, name: 'RubberMan' },
+//   { id: 5, name: 'Dynama' },
+//   { id: 6, name: 'Dr. IQ' },
+//   { id: 7, name: 'Magma' },
+//   { id: 8, name: 'Tornado' }
+// ];
